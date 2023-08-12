244652ed076020a21db40098566d53f47eb7882c
iqbal nurrizqi <iqbalnurrizqi2@gmail.com>
2023-08-12 12:15:17 +0700

    update dark mode + fiture tambahan

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,13 +1,8 @@
-// Hamburger section
+
 
 const hamburger = document.querySelector('#hamburger')
 const navMenu = document.querySelector('#nav-menu')
-
-
-hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle('hamburger-active')
-    navMenu.classList.toggle('hidden')
-})
+const toTop = document.querySelector('#toTop')
 
 // Navbar fixed
 window.onscroll = function() {
@@ -16,8 +11,41 @@ window.onscroll = function() {
 
     if(window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed')
+        toTop.classList.remove('hidden')
+        toTop.classList.add('flex')
     } else {
         header.classList.remove('navbar-fixed')
+        toTop.classList.remove('flex')
+        toTop.classList.add('hidden')
     }
 }
+
+// Hamburger section
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('hamburger-active')
+    navMenu.classList.toggle('hidden')
+})
+
+// klik diluar hamburgernya
+window.addEventListener('click', function(e) {
+    if(e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active')
+        navMenu.classList.add('hidden')  
+    }
+})
+
+
+// dark mode toggle
+const darkMode = document.querySelector('#dark-toggle')
+const html = document.querySelector('html')
+
+darkMode.addEventListener('click', function() {
+    if (darkMode.checked) {
+        html.classList.add('dark');
+        localStorage.theme = ('dark');
+    } else {
+        html.classList.remove('dark');
+        localStorage.theme = ('light');
+    }
+})
 
